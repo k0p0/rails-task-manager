3d7227bf961ce10212678e24a13307845c9f4226
Olivier Bellier <olivier@observices.fr>
2017-08-08 12:32:00 +0200

    complete task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+resources :tasks
+
+  # get 'tasks', to: 'tasks#index'
+
+  # get 'tasks/new', to: 'tasks#new'
+
+  # get 'task/:id', to: 'tasks#show'
+
+  # post 'tasks/create', to: 'task#create'
+
+  # get 'tasks/:id/edit', to: 'task#edit'
+
+  # patch 'tasks/:id', to: 'task#update'
+
+  # delete 'tasks/:id', to: "tasks#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
